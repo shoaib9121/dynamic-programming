285f7a6688682cc8d1937ad69b3d4dfc02c1abac
Shoaib Ahmed <sahmed12@wsgc.com>
2022-07-17 03:08:04 +0500

    code implemented

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
-# Sum all numbers till the given ones
+# Three Sum
 
-Write a function sumTo(n) that calculates the sum of numbers 1 + 2 + ... + n
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+### Example 1
+
+```
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation:
+nums[0] + nums[1] + nums[1] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+```
+
+### Example 2
+
+```
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+```
+
+### Example 3
+
+```
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+```
+
+### Constraints
+
+- `3 <= nums.length <= 3000`
+- `-105 <= nums[i] <= 105`
+
+### Time and Space Complexity
+
+#### Time Complexity
+
+Time complexity is O(n log n) for sorting the input array + O(n^2) since for the two loops. So the overall time complexity is O(n^2)
+
+#### Space Complexity
+
+O(1). No extra space is used
--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-const sumTo = (n) => {
-  if (n === 1) return 1;
-  return n + sumTo(n - 1);
+const threeSum = (nums = []) => {
+  let results = [];
+  nums.sort((a, b) => a - b);
+  // execute outer loop until second last item(length -2) as we won't be having 3 diff pointers available hence stop the algorithm.
+  for (let point1 = 0; point1 < nums.length - 2; point1++) {
+    // skip all duplicates from left, point1 > 0 ensures this check is made only from 2nd element onwards
+    if (point1 > 0 && nums[point1] === nums[point1 - 1]) {
+      continue;
+    }
+
+    let point2 = point1 + 1;
+    let point3 = nums.length - 1;
+
+    while (point2 < point3) {
+      let sum = nums[point1] + nums[point2] + nums[point3];
+
+      if (sum === 0) {
+        results.push([nums[point1], nums[point2], nums[point3]]);
+        point3--;
+
+        // Skip all duplicates from right
+        while (point2 < point3 && nums[point3] === nums[point3 + 1]) point3--;
+      } else if (sum > 0) {
+        point3--;
+      } else {
+        point2++;
+      }
+      // console.log(`sum:$ {sum} pt1:${point1} pt2:${point2} pt3:${point3}`);
+    }
+  }
+  return results;
 };
-console.log(sumTo(5));
+console.log(threeSum([-2, -1, 0, -1, 1]));
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
+console.log(threeSum([0, 0, 0]));
+console.log(threeSum([0, 1, 1]));
