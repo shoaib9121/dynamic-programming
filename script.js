fa2124794c8f076ac42b0274ed6fb7b2d64d6068
Shoaib Ahmad <shoaib9121@yahoo.com>
2022-08-18 15:33:40 +0500

    O(n) solution for squares of sorted array

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 /**
+ * Time complexity is O(n). Calculate squares using two pointers
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortedSquaresOptimalSolution = (nums) => {
+  let posPointer = 0,
+    negPointer = -1;
+  let squares = [];
+  while (posPointer < nums.length && nums[posPointer] < 0) {
+    posPointer++;
+    negPointer = posPointer - 1;
+  }
+
+  while (negPointer >= 0 && posPointer < nums.length) {
+    if (Math.pow(nums[negPointer], 2) < Math.pow(nums[posPointer], 2)) {
+      squares.push(Math.pow(nums[negPointer], 2));
+      negPointer--;
+    } else {
+      squares.push(Math.pow(nums[posPointer], 2));
+      posPointer++;
+    }
+  }
+
+  while (negPointer >= 0) {
+    squares.push(Math.pow(nums[negPointer], 2));
+    negPointer--;
+  }
+
+  while (posPointer < nums.length) {
+    squares.push(Math.pow(nums[posPointer], 2));
+    posPointer++;
+  }
+
+  return squares;
+};
+
+/**
  * Time complexity is O(n^2)
  * @param {number[]} nums
  * @return {number[]}
  */
 const sortedSquares = (nums) => {
-  const squared = nums.map((num) => Math.pow(num, 2)); // n
+  const squared = nums.map((num) => Math.pow(num, 2));
 
   for (let i = 0; i < squared.length; i++) {
     const curr = squared[i];
@@ -24,5 +61,8 @@ const sortedSquares = (nums) => {
   return squared;
 };
 
-console.log(sortedSquares([-4, -1, 0, 3, 10])); // [0,1,9,16,100]
+// console.log(sortedSquaresOptimalSolution([-4, -1, 0, 3, 10])); // [0,1,9,16,100]
+console.log(sortedSquaresOptimalSolution([-7, -3, 2, 3, 11])); // [4,9,9,49,121]
+
+// console.log(sortedSquares([-4, -1, 0, 3, 10])); // [0,1,9,16,100]
 // console.log(sortedSquares([-7, -3, 2, 3, 11])); // [4,9,9,49,121]
