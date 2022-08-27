69becd6516bb1a7e52f6206cd9bd225aa6eb626f
Shoaib Ahmad <shoaib9121@yahoo.com>
2022-08-28 02:38:39 +0500

    done

--- README.md
+++ README.md
@@ -1,29 +1,18 @@
-# River Sizes
+# Pascal to sentence case
 
-You're given a two-dimensional array (a matrix) of potentially unequal height
-and width containing only `0`s and `1`s. Each
-`0` represents land, and each `1` represents part of a
-river. A river consists of any number of `1`s that are either
-horizontally or vertically adjacent (but not diagonally adjacent). The number
-of adjacent `1`s forming a river determine its size.
+Convert pascal case string to sentence.
 
 ### Example 1
 
 ```
 Input:
 
-[
-  [1, 0, 0, 1, 0],
-  [1, 0, 1, 0, 0],
-  [0, 0, 1, 0, 1],
-  [1, 0, 1, 0, 1],
-  [1, 0, 1, 1, 0],
-]
+ThisIsPascalCase
 ```
 
 ```
 Output:
 
-[1, 2, 2, 2, 5]
+This is pascal case
 
 ```
--- script.js
+++ script.js
@@ -1,69 +1,18 @@
-/**
- * Time complexity is O(n*m)
- * @param {matrix[][]} matrix
- * @return {number[]}
- */
-const twoDMatrix = (matrix) => {
-  let riverSizes = [];
-  let visitedMatrix = matrix.map((row) => row.map(() => false)); // auxilary/additional data structure to set default visited with false
-
-  for (let row = 0; row < matrix.length; row++) {
-    for (let col = 0; col < matrix[row].length; col++) {
-      let river = { size: 0 };
-      traverseRiver(matrix, row, col, visitedMatrix, river);
-      if (river.size > 0) {
-        riverSizes.push(river.size);
+const pascalToSentenceCase = (str) => {
+  for (let i = 0; i < str.length - 1; i++) {
+    let char = str.charAt(i);
+    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      if (i > 0) {
+        str = replaceCharAt(str, i, char.toLowerCase());
       }
     }
   }
-  return riverSizes;
+  return str;
 };
 
-const traverseRiver = (matrix, row, col, visitedMatrix, river) => {
-  if (matrix[row][col] === 0) return;
-  if (isVisited(visitedMatrix, row, col)) return;
-  visitedMatrix[row][col] = true;
-  river.size++;
-
-  const neighbours = getNeighbours(matrix, row, col);
-
-  for (const [nbrRow, nbrCol] of neighbours) {
-    traverseRiver(matrix, nbrRow, nbrCol, visitedMatrix, river);
-  }
+const replaceCharAt = (str, index, replacement) => {
+  return str.substring(0, index) + " " + replacement + str.substring(index + 1);
 };
-
-/**
- * If node visited already
- * @param {number[][]} matrix
- * @param {number} row
- * @param {number} col
- * @returns boolean
- */
-const isVisited = (matrix, row, col) => {
-  return matrix[row][col] ? true : false;
-};
-
-const getNeighbours = (matrix, row, col) => {
-  let neighbours = [];
-  if (row !== 0) neighbours.push([row - 1, col]);
-  if (row !== matrix.length - 1) neighbours.push([row + 1, col]);
-  if (col !== 0) neighbours.push([row, col - 1]);
-  if (col !== matrix[row].length - 1) neighbours.push([row, col + 1]);
-  return neighbours;
-};
-
-const largeMatrix = [
-  [1, 0, 0, 1, 0],
-  [1, 0, 1, 0, 0],
-  [0, 0, 1, 0, 1],
-  [1, 0, 1, 0, 1],
-  [1, 0, 1, 1, 0],
-];
-
-const smallMatrix = [
-  [1, 0, 0],
-  [1, 0, 1],
-  [0, 1, 0],
-];
-console.log(twoDMatrix(largeMatrix)); // [2,1,5,2,2]
-console.log(twoDMatrix(smallMatrix)); // [2,1,1]
+// console.log(`a-z ${"a".charCodeAt(0)} ${"z".charCodeAt(0)}`);
+// console.log(`A-Z ${"A".charCodeAt(0)} ${"Z".charCodeAt(0)}`);
+console.log(pascalToSentenceCase("ThisIsPascalCase")); // This is pascal case
