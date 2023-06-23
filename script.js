89edba3454b190ed1f9c24d5c539345b70d2e073
Shoaib Ahmad <shoaib9121@yahoo.com>
2023-06-23 19:04:53 +0500

    reverse array without special characters

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-const functionName = (s) => {
-  return s;
+// Reverse an array without reversing special characters
+// Input ['a', '$', 'b', '#', 'c'];
+// Output : ['c', '$', 'b', '#', 'a']
+
+// Input : ['b', 'a', '$', 'c', '#']
+// Output : ['c', 'a', '$', 'b', '#']
+
+const isCharInRange = (char) => {
+  const isValid = char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122;
+  return isValid;
 };
 
-console.log(functionName("str"));
+const swap = (arr, currP, lastP) => {
+  let temp;
+  temp = arr[currP];
+  arr[currP] = arr[lastP];
+  arr[lastP] = temp;
+};
+
+const isSpecialChar = (char) => {
+  return !/[a-zA-z]/.test(char);
+};
+
+const reverse = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    if (isSpecialChar(arr[left])) {
+      left++;
+    } else if (isSpecialChar(arr[right])) {
+      right--;
+    } else {
+      swap(arr, left, right);
+      left++;
+      right--;
+    }
+  }
+
+  return arr;
+};
+
+console.log(reverse(["b", "a", "$", "c", "#"])); // ["c","a","$","b","#"]
+console.log(reverse(["a", "$", "b", "#", "c"])); // ["c","$","b","#","a"]
