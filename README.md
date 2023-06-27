f766dcd77431530c1c561133b135994b9ce988d9
Shoaib Ahmad <shoaib9121@yahoo.com>
2023-06-27 18:38:41 +0500

    more anagram

--- README.md
+++ README.md
@@ -3,9 +3,17 @@
 Given an array of strings, group the anagrams together. An anagram is a word or phrase formed by rearranging the letters of another word or phrase.
 
 ```
-Input: ops = ["eat", "tea", "tan", "ate", "nat", "bat"]
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"]
 ```
 
 ```
 Output: [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]
+```
+
+Find if two string are anagram
+
+```
+str = 'EAT'
+str1 = 'TEA', 'ATE', 'EAT', 'ETA'
+str is anagram to str1
 ```
--- script.js
+++ script.js
@@ -31,3 +31,53 @@ const groupAnaGrams = (strs) => {
 const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 console.log("Using simple object", groupAnaGrams(strs));
 console.log("Using new Map()", groupAnaGramsWithMap(strs));
+
+/**
+ * Find if two string are anagram
+ * str = 'EAT' str1 = `TEA`, 'ATE', 'EAT', 'ETA'
+ * str is anagram to str1
+ *
+ */
+
+// also EAT != ATEE
+// Solution with sorting
+function checkAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  const sortedstr1 = str1.split("").sort().join("");
+  const sortedstr2 = str2.split("").sort().join("");
+  if (sortedstr1 === sortedstr2) {
+    return true;
+  }
+  return false;
+}
+// Solution without sorting
+function checkAnagram(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  const map1 = new Map();
+  const map2 = new Map();
+  for (let i = 0; i < str1.length; i++) {
+    if (map1.has(str1[i])) {
+      map1.set(str1[i], map.get(str1[i]) + 1);
+    } else {
+      map1.set(str1[i], 1);
+    }
+    if (map2.has(str2[i])) {
+      map2.set(str2[i], map.get(str2[i]) + 1);
+    } else {
+      map2.set(str2[i], 1);
+    }
+  }
+  let flag = true;
+  map1.forEach((value, key) => {
+    if (map2.get(key) !== value) {
+      flag = false;
+    }
+  });
+  return flag;
+}
+console.log(">>>>>1>>>>", checkAnagram("EAT", "TAE"));
+console.log(">>>>>2>>>>", checkAnagram("EAT", "ATEE"));
