a611ae3fb8a06ead887525fb227ede538358ef6f
Shoaib Ahmad <shoaib9121@yahoo.com>
2023-07-10 18:10:34 +0500

    tweaks

--- script.js
+++ script.js
@@ -53,31 +53,35 @@ function checkAnagram(str1, str2) {
   return false;
 }
 // Solution without sorting
-function checkAnagram(str1, str2) {
-  if (str1.length !== str2.length) {
-    return false;
-  }
-  const map1 = new Map();
-  const map2 = new Map();
+function checkAnagramWithoutSorting(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  let map1 = new Map();
+  let map2 = new Map();
+
   for (let i = 0; i < str1.length; i++) {
     if (map1.has(str1[i])) {
-      map1.set(str1[i], map.get(str1[i]) + 1);
+      map1.set(str1[i], map1.get(str1[i]) + 1);
     } else {
       map1.set(str1[i], 1);
     }
+  }
+  for (let i = 0; i < str2.length; i++) {
     if (map2.has(str2[i])) {
-      map2.set(str2[i], map.get(str2[i]) + 1);
+      map2.set(str2[i], map2.get(str2[i]) + 1);
     } else {
       map2.set(str2[i], 1);
     }
   }
-  let flag = true;
+
+  let isValid = true;
   map1.forEach((value, key) => {
     if (map2.get(key) !== value) {
-      flag = false;
+      isValid = false;
     }
   });
-  return flag;
+  return isValid;
 }
-console.log(">>>>>1>>>>", checkAnagram("EAT", "TAE"));
-console.log(">>>>>2>>>>", checkAnagram("EAT", "ATEE"));
+console.log("With sorting", checkAnagram("EAT", "TAE")); // true
+console.log("With sorting", checkAnagram("EAT", "ATEE")); // false
+console.log("Without sorting", checkAnagramWithoutSorting("EAT", "TAE")); // true
